c97d6f59c5daa5d329924417904fb9c2ee002719
Matheus Nicolay <58345766+matheus-nicolay@users.noreply.github.com>
2023-11-13 15:01:39 -0300

    Alterado para formato de serviço do pfSense

--- ipsec-ping.sh
+++ ipsec-ping.sh
@@ -1,17 +1,55 @@
 #!/bin/sh
 
-# PROVIDE: ipsec-ping
-# REQUIRE: NETWORKING
-# KEYWORD: shutdown
-
-. /etc/rc.subr
-
 name="ipsec-ping"
-rcvar="ipsec_ping_enable"
+command="/root/${name}-script.sh"
+pidfile="/root/${name}.pid"
 
-start_cmd="/root/ipsec-ping.sh"
+rc_start() {
+	# Make sure all process are stopped
+	rc_stop
 
-load_rc_config $name
-: ${ipsec_ping_enable:=no}
+	# Start 
+	${command} & pidnum="$(/bin/pgrep $name)
 
-run_rc_command "$1"
+	if [ -n "${pidnum}" ]; then
+		echo "ipsec-ping started (${pidnum})"
+		/usr/bin/logger -p daemon.info -t ipsec-ping "ipsec-ping started"
+	else
+		echo "ipsec-ping failed to start"
+		/usr/bin/logger -p daemon.info -t ipsec-ping "ipsec-ping failed to start"
+	fi
+}
+
+rc_stop() {	
+	pidnum="$(/bin/pgrep $name)"
+	if [ -n "${pidnum}" ]; then
+		/usr/bin/killall $name
+		echo "ipsec-ping stopped (${pidnum})"
+		/usr/bin/logger -p daemon.info -t ipsec-ping "ipsec-ping stopped"
+	fi
+}
+
+rc_status() {	
+	pidnum="$(/bin/pgrep $name)"
+	if [ -n "${pidnum}" ]; then
+		echo "ipsec-ping is running (${pidnum})"
+	else
+		echo "ipsec-ping is not running"
+	fi
+}
+
+case $1 in
+	start)
+		rc_start
+		;;
+	stop)
+		rc_stop
+		;;
+	restart)
+		rc_stop
+		rc_start
+		;;
+	status)
+		rc_status
+		;;
+esac
