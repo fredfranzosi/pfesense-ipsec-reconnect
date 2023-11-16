d7a4afcfdacc132f8799b12b596148034231b6b5
Matheus Nicolay <58345766+matheus-nicolay@users.noreply.github.com>
2023-11-16 17:03:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,6 +33,6 @@ sed '/<\/acme>/r /tmp/temp_service.xml' /conf/config.xml > /conf/config.xml.tmp 
 
 rm /tmp/temp_service.xml
 
-service ipsec_ping.sh start
+service ipsec_ping.sh start &
 
 echo "Serviço criado"
