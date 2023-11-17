896bd7bc014885e0f59a2eec8bebe873cc0176aa
Matheus Nicolay <58345766+matheus-nicolay@users.noreply.github.com>
2023-11-17 11:25:15 -0300

    Alterado para criação do crontab independente da existência da IPSec

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ source_ip="$(ifconfig -v vtnet1 | grep -o 'inet [^ ]*' | cut -f2 -d' ')"
 #Verificar se existe configuração de IPSec
 check_ipsec=$(/usr/local/sbin/ipsec status)
 
+#Colocar serviço na configuração de inicialização do pfSense apenas se o parâmetro "install" for passado
+if [ "$1" = "install" ]; then
+    echo '<service><name>ipsec_ping</name><rcfile>ipsec_ping.sh</rcfile><executable>ipsec_ping</executable></service>' > /tmp/temp_service.xml
+
+    sed '/<\/acme>/r /tmp/temp_service.xml' /conf/config.xml > /conf/config.xml.tmp && mv /conf/config.xml.tmp /conf/config.xml
+
+    rm /tmp/temp_service.xml
+
+    (crontab -l ; echo "0 0 * * * /root/install.sh") | crontab -
+fi
+
 if [ -n "$check_ipsec" ]; then
     # Comando para obter sub-redes modificadas
     subnets=$(/usr/local/sbin/ipsec status | awk '/con[0-9]+{[0-9]+}/ {gsub(/reqid|SPIs:|,|[a-zA-Z_]/, ""); print $4, $7}' | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'\// | sort | uniq | awk -F'/' '{gsub(/0$/,"1",$1); gsub(/0$/,"254",$1); print $1; gsub(/1$/,"254",$1); print $1}')
@@ -32,17 +43,6 @@ if [ -n "$check_ipsec" ]; then
 
     fetch -o /usr/local/etc/rc.d/ipsec_ping.sh https://raw.githubusercontent.com/matheus-nicolay/pfesense-ipsec-reconnect/main/ipsec_ping.sh 
     chmod +x /usr/local/etc/rc.d/ipsec_ping.sh
-
-    #Colocar serviço na configuração de inicialização do pfSense apenas se o parâmetro "install" for passado
-    if [ "$1" = "install" ]; then
-        echo '<service><name>ipsec_ping</name><rcfile>ipsec_ping.sh</rcfile><executable>ipsec_ping</executable></service>' > /tmp/temp_service.xml
-
-        sed '/<\/acme>/r /tmp/temp_service.xml' /conf/config.xml > /conf/config.xml.tmp && mv /conf/config.xml.tmp /conf/config.xml
-
-        rm /tmp/temp_service.xml
-
-        (crontab -l ; echo "0 0 * * * /root/install.sh") | crontab -
-    fi
 
     #inicia o serviço
     service ipsec_ping.sh start
